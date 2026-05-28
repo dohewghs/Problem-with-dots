4bbf1a96f76365158748754e3da4f18a238cafe5
dohewghs <dohewghs@gmail.com>
2026-05-28 22:42:58 +0300

    make preset1 in pointsgenerator

--- Project1/PointsGenerator.h
+++ Project1/PointsGenerator.h
@@ -23,7 +23,7 @@ private:
 
 	Point changePoint(const Point& point)
 	{
-		double range = getNumInRange(0.0, 20.0);
+		double range = getNumInRange(0.0, 50.0);
 
 		Point res(point);
 
@@ -43,12 +43,33 @@ public:
 
 		for (const Point& point : congestion_points)
 		{
-			int count_new_points = static_cast<int>(getNumInRange(0, 20));
+			int count_new_points = static_cast<int>(getNumInRange(20, 30));
 			
 			for (int i=0; i<count_new_points; ++i)
 				res_points.AddPoint(changePoint(point));
 		}
 
 		return res_points;
+	}
+
+	Points get_points_preset1(int height, int width)
+	{
+		Points dots;
+		int n = 20;
+		int m = 20;
+
+		int step_y = height / m;
+		int step_x = width / n;
+
+		for (int i = 1; i <= n; ++i)
+		{
+			for (int j = 1; j <= m; ++j)
+			{
+				dots.AddPoint(Point(step_x * j, step_y * i, 1, color(0, 0, 0)));
+			}
+		}
+		Points points = this->genPoints(dots);
+
+		return points;
 	}
 };
