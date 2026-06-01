867ccb8e9300f31a688a8748399b3c8d3653b184
dohewghs <dohewghs@gmail.com>
2026-06-01 21:00:46 +0300

    some preset for generation points

--- Project1/PointsGenerator.h
+++ Project1/PointsGenerator.h
@@ -52,7 +52,7 @@ public:
 		return res_points;
 	}
 
-	Points get_points_preset1(int height, int width)
+	Points get_points_preset1(int width, int height)
 	{
 		Points dots;
 		int n = 20;
@@ -71,5 +71,55 @@ public:
 		Points points = this->genPoints(dots);
 
 		return points;
+	}
+
+	Points get_points_preset2(int height, int width)
+	{
+		// height = width
+		Points dots;
+
+		// Базові межі нашої тестової зони (ідеальний квадрат)
+		double startX = 100.0;
+		double startY = 100.0;
+		double zoneSize = height; // Ширина і висота
+
+		// 1. Створюємо фоновий випадковий шум (рівномірно по всій зони)
+		// Це перевірить, як алгоритм ігнорує поодинокі точки
+		for (int i = 0; i < 500; ++i) {
+			// std::rand() дає число від 0 до RAND_MAX. Переводимо в діапазон [0, zoneSize]
+			double rx = startX + (static_cast<double>(std::rand()) / RAND_MAX) * zoneSize;
+			double ry = startY + (static_cast<double>(std::rand()) / RAND_MAX) * zoneSize;
+			dots.AddPoint(Point(rx, ry, 0.0, color(0, 0, 0, 255)));
+		}
+
+		// 2. Створюємо 4 чіткі скупчення (кластери) точок.
+		// Ми знаємо їхні точні математичні центри:
+		// Кластер 1: (200, 200)
+		// Кластер 2: (400, 200)
+		// Кластер 3: (200, 400)
+		// Кластер 4: (400, 400)
+
+		double clusterCenters[4][2] = {
+		  {200.0, 200.0},
+		  {400.0, 200.0},
+		  {200.0, 400.0},
+		  {400.0, 400.0}
+		};
+
+		// Накидаємо по 50 точок щільно навколо кожного центру (в радіусі 15 пікселів)
+		for (int c = 0; c < 4; ++c) {
+			double cx = clusterCenters[c][0];
+			double cy = clusterCenters[c][1];
+
+			for (int p = 0; p < 50; ++p) {
+				// Випадковий зсув від центру в межах [-15, 15]
+				double offsetX = ((static_cast<double>(std::rand()) / RAND_MAX) * 30.0) - 15.0;
+				double offsetY = ((static_cast<double>(std::rand()) / RAND_MAX) * 30.0) - 15.0;
+
+				dots.AddPoint(Point(cx + offsetX, cy + offsetY, 0.0, color(0, 0, 0, 255)));
+			}
+		}
+
+		return dots;
 	}
 };
