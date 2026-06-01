89012c7b6d1dce2a0322015d06c3ab1ad32da1f1
dohewghs <dohewghs@gmail.com>
2026-06-01 21:01:33 +0300

    visual changes, add gui

---
+++ Project1/GuiManager.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "AppConfig.h"
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h> // або інший бекенд, який ти використовуєш
+
+class GuiManager
+{
+public:
+    GuiManager(SDL_Window* window, SDL_Renderer* renderer)
+    {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui::StyleColorsDark();
+        ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+        ImGui_ImplSDLRenderer3_Init(renderer);
+    }
+
+    ~GuiManager() 
+    {
+        ImGui_ImplSDLRenderer3_Shutdown();
+        ImGui_ImplSDL3_Shutdown();
+        ImGui::DestroyContext();
+    }
+
+    void processEvent(const SDL_Event& event) 
+    {
+        ImGui_ImplSDL3_ProcessEvent(&event);
+    }
+
+    void drawInterface(AppConfig& config) 
+    {
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // Вікно налаштувань алгоритму
+        ImGui::Begin("Algorithm Settings");
+
+        ImGui::Text("Grid Parameters:");
+        ImGui::SliderInt("Zones X", &config.zones_x, 5, 200);
+        ImGui::SliderInt("Zones Y", &config.zones_y, 5, 200);
+
+        ImGui::Separator();
+        ImGui::Text("Cluster Parameters:");
+        ImGui::SliderFloat("Search Radius", &config.radius, 1.0f, 100.0f, "%.1f");
+        ImGui::SliderInt("Min Points", &config.min_points_for_strong, 1, 20);
+
+        ImGui::Separator();
+        ImGui::Checkbox("Show Grid Lines", &config.show_grid);
+
+        ImGui::Separator();
+        ImGui::SliderFloat("Scale factor", &config.scale, 1.0f, 3.0f, "%.1f");
+
+        if (ImGui::Button("Regenerate Points")) 
+        {
+            config.regenerate_points = true;
+        }
+
+        ImGui::End();
+        ImGui::Render();
+    }
+
+    void render(SDL_Renderer* renderer) 
+    {
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+    }
+};
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,25 +1,133 @@
-#include "WindowRenderer.h"
 #include "Points.h"
 #include "PointsGenerator.h"
 #include "Checker.h"
+#include "AppConfig.h"
+#include <SDL3/SDL.h>
+#include "GuiManager.h"
+
+void SDL_RenderPoint(SDL_Renderer* renderer, Point pt, float size = 1);
+void SDL_RenderPoints(SDL_Renderer* renderer, const Points& points, float size = 1);
+void drawGridLines(SDL_Renderer* renderer, const Zone& main_zone, int zones_x, int zones_y);
 
 int main()
 {
 	int width = 800;
-	int height = 600;
+	int height = 800;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("test", width, height, 0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+	AppConfig config;
+	GuiManager gui(window, renderer);
+	Zone mainSurface(0, 0, width, height);
+	Checker checker(mainSurface);
 	PointsGenerator gen(std::random_device{}());
 
-	Points points = gen.get_points_preset1(width, height);
+	Points points = gen.get_points_preset2(width, height);
 
-	Checker checker(Zone(0, 0, 800, 600));
-	checker.checkPoints(points);
-
-	WindowRenderer renderer(points);
-
-	if (renderer.Init(L"Points Renderer", width, height)) 
+	bool isRunning = true;
+	while (isRunning)
 	{
-		renderer.Run();
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			gui.processEvent(event); // Передаємо події в ImGui
+			if (event.type == SDL_EVENT_QUIT)
+			{
+				isRunning = false;
+			}
+		}
+
+		gui.drawInterface(config);
+
+		if (config.regenerate_points) 
+		{
+			//original_dots = generateTestPoints();
+			config.regenerate_points = false;
+		}
+
+		Points working_dots = points; // Скидаємо старі центри перед перерахунком
+		
+		Points weakAvg = checker.weakAverages(points, config);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderScale(renderer, config.scale, config.scale);
+
+		if (config.show_grid) 
+		{
+			drawGridLines(renderer, mainSurface, config.zones_x, config.zones_y);
+		}
+
+		SDL_RenderPoints(renderer, points, 2);
+		SDL_RenderPoints(renderer, weakAvg, 2);
+
+		SDL_SetRenderScale(renderer, 1, 1);
+		gui.render(renderer);
+
+		SDL_RenderPresent(renderer);
 	}
 
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
+}
+
+void SDL_RenderPoint(SDL_Renderer* renderer, Point pt, float pointSize)
+{
+	SDL_SetRenderDrawColor(renderer, pt.col.R, pt.col.G, pt.col.B, pt.col.A);
+
+	if (pointSize <= 1)
+	{
+		SDL_RenderPoint(renderer, pt.x, pt.y);
+	}
+	else
+	{
+		SDL_FRect rect{
+		pt.x - pointSize / 2,
+		pt.y - pointSize / 2,
+		pointSize,
+		pointSize
+		};
+
+		SDL_RenderFillRect(renderer, &rect);
+	}
+}
+
+void SDL_RenderPoints(SDL_Renderer* renderer, const Points& points, float point_size)
+{
+	for (const auto& pt : points)
+	{
+		SDL_RenderPoint(renderer, pt, point_size);
+	}
+}
+
+void drawGridLines(SDL_Renderer* renderer, const Zone& main_zone, int zones_x, int zones_y)
+{
+	double size_x = main_zone.length / zones_x;
+	double size_y = main_zone.width / zones_y;
+
+	SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+
+	for (int i = 0; i <= zones_x; ++i)
+	{
+		float x = static_cast<float>(main_zone.x + i * size_x);
+		float y1 = static_cast<float>(main_zone.y);
+		float y2 = static_cast<float>(main_zone.y + main_zone.width);
+
+		SDL_RenderLine(renderer, x, y1, x, y2);
+	}
+
+	for (int j = 0; j <= zones_y; ++j)
+	{
+		float y = static_cast<float>(main_zone.y + j * size_y);
+		float x1 = static_cast<float>(main_zone.x);
+		float x2 = static_cast<float>(main_zone.x + main_zone.length);
+
+		SDL_RenderLine(renderer, x1, y, x2, y);
+	}
 }
